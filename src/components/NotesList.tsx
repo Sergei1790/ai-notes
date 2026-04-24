5ee955742efabab0e7ad582ca47cb7586249d33c
Sergei1790 <lute1790@gmail.com>
2026-04-24 09:28:51 +0300

    feat: AI summary and tags via Gemini

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,5 +1,5 @@
 import {Note} from '@/generated/prisma/client';
-import {deleteNote} from '@/lib/actions';
+import {deleteNote, generateSummary, generateTags} from '@/lib/actions';
 export default async function NotesList({notes}: {notes: Note[]}) {
     return (
         <div className="p-6">
@@ -9,9 +9,21 @@ export default async function NotesList({notes}: {notes: Note[]}) {
                     <div key={note.id}>
                         {note.title} <br />
                         {note.content}
+                        {note.tags && <p className="text-sm text-muted mt-2">{note.tags}</p>}
+                        {note.summary && <p className="text-sm text-muted mt-2">{note.summary}</p>}
                         <form action={deleteNote.bind(null, note.id)}>
                             <button type="submit" className="cursor-pointer w-full bg-primary text-white px-4 py-2 rounded-xl">
                                 Delete Note
+                            </button>
+                        </form>
+                        <form action={generateTags.bind(null, note.id)}>
+                            <button type="submit" className="cursor-pointer w-full bg-primary text-white px-4 py-2 rounded-xl">
+                                Generate Ai Tags
+                            </button>
+                        </form>
+                        <form action={generateSummary.bind(null, note.id)}>
+                            <button type="submit" className="cursor-pointer w-full bg-primary text-white px-4 py-2 rounded-xl">
+                                Generate Ai Summary
                             </button>
                         </form>
                     </div>
